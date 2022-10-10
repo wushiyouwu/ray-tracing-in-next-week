f97840e2af1aedc621c8942b3c68b12a09cc9245
wushiyouwu <96167822+wushiyouwu@users.noreply.github.com>
2022-10-10 14:16:10 +0800

    Add files via upload

---
+++ ray-tracing-in-one-weekend-master/matreial.h
@@ -0,0 +1,114 @@
+#ifndef METERIAL_H
+#define METERIAL_H
+
+#include"rtweekend.h"
+#include"texture.h"
+
+struct hit_record;
+
+class material {
+public:
+	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)const = 0;
+	virtual color emitted(double u, double v, const point3& p) const 
+	{ return color(0, 0, 0);}
+};
+
+class lambertian :public material {
+public:
+	lambertian(const color& a) :albedo(make_shared<solid_color>(a)) {}
+	lambertian(shared_ptr<texture> a):albedo(a){}
+	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)const override {
+		auto scatter_direction = rec.normal + random_unit_vector();
+
+		//Catch degenerate scatter direction;
+		if (scatter_direction.near_zero())
+			scatter_direction = rec.normal;
+		scattered = ray(rec.p, scatter_direction,r_in.time());
+		attenuation = albedo->value(rec.u,rec.v,rec.p);
+		return true;
+	}
+public:
+	shared_ptr<texture> albedo;
+};
+
+class metal :public material {
+public:
+	metal(const color& a,double f) :albedo(a),fuzz(f<1?f:1) {};
+	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)const override {
+		vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+		scattered = ray(rec.p, reflected+fuzz*random_in_unit_sphere(), r_in.time());
+		attenuation = albedo;
+		return (dot(scattered.direction(),rec.normal)>0);
+	}
+public:
+	color albedo;
+	double fuzz;
+};
+
+class dielectric :public material {
+public:
+	dielectric(double index_of_refraction) :ir(index_of_refraction) {};
+
+	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)const override {
+		attenuation = color(1.0, 1.0, 1.0);
+		double refraction_ratio = rec.front_face ? (1.0 / ir):ir;
+
+		vec3 unit_direciton = unit_vector(r_in.direction());
+		double cos_theta = fmin(dot(-unit_direciton, rec.normal), 1.0);
+		double sin_theta = sqrt(1 - cos_theta * cos_theta);
+		
+		bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+		vec3 direction;
+
+		if (cannot_refract||reflectance(cos_theta,refraction_ratio)>random_double())
+			direction = reflect(unit_direciton, rec.normal);
+		else
+			direction = refract(unit_direciton, rec.normal, refraction_ratio);
+	
+		scattered = ray(rec.p, direction, r_in.time());
+		return true;
+	}
+public:
+	double ir;//Index of Refraction
+private:
+	static double reflectance(double cosine, double ref_index) {
+		//Use Schlick's approximation for reflectance.
+		auto r0 = (1 - ref_index) / (1 + ref_index);
+		r0 = r0 * r0;
+		return r0 + (1 - r0) * pow((1 - cosine), 5);
+	}
+};
+
+class diffuse_light :public material {
+public:
+	diffuse_light(shared_ptr<texture> a):emit(a){}
+	diffuse_light(color c):emit(make_shared<solid_color>(c)){}
+
+	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)const override { return false; }
+	virtual color emitted(double u, double v, const point3& p)const override {
+		return emit->value(u, v, p);
+	}
+
+private:
+	shared_ptr<texture> emit;
+};
+
+class isotropic :public material {
+public:
+	isotropic(color c) :albedo(make_shared<solid_color>(c)) {}
+	isotropic(shared_ptr<texture> a):albedo(a){}
+
+
+	virtual bool scatter(
+		const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+	) const override {
+		scattered = ray(rec.p, random_in_unit_sphere(), r_in.time());
+		attenuation = albedo->value(rec.u, rec.v, rec.p);
+		return true;
+	}
+
+
+public:
+	shared_ptr<texture> albedo;
+};
+#endif 
---
+++ ray-tracing-in-one-weekend-master/rtweekend.h
@@ -0,0 +1,50 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include<cmath>
+#include<limits>
+#include<memory>
+#include<cstdlib>
+
+//using
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+//Constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+//Utility Function
+inline double degrees_to_radian(double degrees){
+	return degrees * pi / 180;
+}
+
+inline double random_double() {
+	//return a random real in [0,1)
+	return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+	//returns a random real in [min,max)
+	return min + (max - min) * random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+	if (x < min)return min;
+	if (x > max)return max;
+	return x;
+}
+
+inline int random_int(int min, int max) {
+	return static_cast<int>(random_double(min, max + 1));
+}
+
+//Common Headers
+
+#include"ray.h"
+#include"vec3.h"
+
+#endif
+
